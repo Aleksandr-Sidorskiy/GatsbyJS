2d25b7504b1a3e0cc3f74e0066a49be346c417d7
Aleksandr-Sidorskiy <sanu43967@gmail.com>
2023-03-06 20:06:39 +0200

    Link

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,17 @@
-import React from "react"
-import Header from "../components/header/header"
+import * as React from "react"
+import { Link } from "gatsby"
+// import Header from "../components/header/header"
 
 const HomePage = () => {
   return (
-    <>
-      <Header />
-    </>
+    <main>
+      <h1>Welcome to my Gatsby site!</h1>
+      <Link to="/about">About</Link>
+      <p>I'm making this by following the Gatsby Tutorial.</p>
+    </main>
   )
 }
+
+export const Head = () => <title>Home Page</title>
 
 export default HomePage
