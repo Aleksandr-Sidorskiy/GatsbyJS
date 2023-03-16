844ca83dd2463bbdbda23b32b1e252d2d48bc50d
Aleksandr-Sidorskiy <you@example.comsanu43967@gmail.com>
2023-03-16 21:33:05 +0200

    Finished Gatsby Tutorial Part 3

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,15 +1,20 @@
 module.exports = {
+  siteMetadata: {
+    title: "My First Gatsby Site",
+  },
   plugins: [
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: `${__dirname}/src/images/`,
-      },
-    },
-    // add style plugins below
-    `gatsby-plugin-typography`,
-    `gatsby-plugin-sass`,
-    `gatsby-plugin-styled-components`,
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+    // {
+    //   resolve: `gatsby-source-filesystem`,
+    //   options: {
+    //     name: `images`,
+    //     path: `${__dirname}/src/images/`,
+    //   },
+    // },
+    // // add style plugins below
+    // `gatsby-plugin-typography`,
+    // `gatsby-plugin-sass`,
+    // `gatsby-plugin-styled-components`,
   ],
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import * as React from "react"
+import { StaticImage } from "gatsby-plugin-image"
 import Layout from "../components/layout/layout"
 import Container from "../components/wrapper/wrapper-style"
 const HomePage = () => {
@@ -6,6 +7,10 @@ const HomePage = () => {
     <Container>
       <Layout pageTitle="Home Page">
         <p>I'm making this by following the Gatsby Tutorial.</p>
+        <StaticImage
+          alt="Clifford, a reddish-brown pitbull, posing on a couch and looking stoically at the camera"
+          src="../images/coffee-cup.jpg"
+        />
       </Layout>
     </Container>
   )
